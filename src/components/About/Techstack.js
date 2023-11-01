d30b3da8bc6e032705e54c3a9cbc21c2ec0902c0
vijay5599 <vbvijay84@gmail.com>
2023-11-01 15:42:40 +0530

    my portfolio

---
+++ src/components/About/Techstack.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { ImHtmlFive } from "react-icons/im";
+import { FaCss3Alt } from "react-icons/fa";
+import { SiExpress } from "react-icons/si";
+
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiMongodb,
+  DiPython,
+  DiGit,
+} from "react-icons/di";
+import { SiFirebase, SiTailwindcss, SiBootstrap, SiMui } from "react-icons/si";
+
+function Techstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <ImHtmlFive />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaCss3Alt />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiExpress />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiMongodb />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTailwindcss />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiBootstrap />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMui />
+      </Col>
+    </Row>
+  );
+}
+
+export default Techstack;
---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import { SiVisualstudiocode, SiNetlify } from "react-icons/si";
+
+import { FaWindows } from "react-icons/fa";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaWindows />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNetlify />
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
